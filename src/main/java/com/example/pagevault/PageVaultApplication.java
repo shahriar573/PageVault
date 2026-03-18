b9e0d084341dcd732decf826270873754015ec25
Shahriar Aziz Khan <khan2107034@stud.kuet.ac.bd>
2026-03-18 13:25:06 +0600

    Initial commit for PageVault

---
+++ src/main/java/com/example/pagevault/PageVaultApplication.java
@@ -0,0 +1,13 @@
+package com.example.pagevault;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PageVaultApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageVaultApplication.class, args);
+    }
+
+}
